dbc5eb0b4fd7d0aa5d39793d0a8bd2446fe50bdf
jigneshrb21 <jigneshrb21@gmail.com>
2026-07-27 00:04:58 +0530

    Time: 1 ms (10.14%), Space: 47.5 MB (85.68%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,21 @@
+class Solution {
+    private double power(double x, long n){
+        if(n == 0 || x == 1){
+            return 1.0;
+        }
+        if(n == 1){
+            return x;
+        }
+        if(n % 2 == 0){
+            return power(x * x, n / 2);
+        }
+        return x * power(x, n - 1);
+   }
+    public double myPow(double x, int N) {
+        long n = N;
+        if(n < 0){
+            return 1.0 / power(x, -n);
+        }
+        return power(x, n);
+    }
+}
