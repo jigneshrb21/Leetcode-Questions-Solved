fa98fe4e853e0c69f36c5e529a7a243071439b8d
jigneshrb21 <jigneshrb21@gmail.com>
2026-08-03 03:00:15 +0530

    Time: 3 ms (95.94%), Space: 52.3 MB (18.2%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -9,12 +9,14 @@ class Solution {
             if(nums[r] == 0){
                 zeros++;
             }
-            while(zeros > k){
+            if(zeros > k){
                 if(nums[l] == 0) zeros--;
                 l++;
             }
-            int len = r - l + 1;
-            maxlen = Math.max(maxlen,len);
+            if(zeros <= k){
+                int len = r - l + 1;
+                maxlen = Math.max(maxlen,len);
+            }
             r++;
         }
         return maxlen;
