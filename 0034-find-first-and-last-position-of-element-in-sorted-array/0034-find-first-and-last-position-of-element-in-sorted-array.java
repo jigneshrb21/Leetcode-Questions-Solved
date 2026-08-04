5b52fd92335119fc15718e9575c7facdda047c7e
jigneshrb21 <jigneshrb21@gmail.com>
2026-08-04 22:32:17 +0530

    Time: 0 ms (100%), Space: 48.2 MB (32.52%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -2,48 +2,42 @@ class Solution {
     public int[] searchRange(int[] nums, int target) {
         int first = firstOccur(nums,target);
 
-        if(first == -1){
+        if(first == nums.length || nums[first] != target){
             return new int[]{-1,-1};
         }
 
-        int last = lastOccur(nums,target);
+        int last = lastOccur(nums,target) - 1;
 
         return new int[]{first,last};
     }
     private int firstOccur(int[] nums, int target){
         int low = 0;
-        int first = -1;
+        int first = nums.length;
         int high = nums.length - 1;
         while(low <= high){
-            int mid = low + (high - low) / 2;
-            if(nums[mid] == target){
+            int mid = (low + high) / 2;
+            if(nums[mid] >= target){
                 first = mid;
                 high = mid - 1;
             }
-            else if(nums[mid] < target){
-                low = mid + 1;
-            }
             else{
-                high = mid - 1;
+                low = mid + 1;
             }
         }
         return first;
     }
     private int lastOccur(int[] nums, int target){
         int low = 0;
-        int last = -1;
+        int last = nums.length;
         int high = nums.length - 1;
         while(low <= high){
-            int mid = low + (high - low) / 2;
-            if(nums[mid] == target){
+            int mid = (low + high) / 2;
+            if(nums[mid] > target){
                 last = mid;
-                low = mid + 1;
-            }
-            else if(nums[mid] < target){
-                low = mid + 1;
+                high = mid - 1;
             }
             else{
-                high = mid - 1;
+                low = mid + 1;
             }
         }
         return last;
