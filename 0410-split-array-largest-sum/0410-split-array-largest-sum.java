5f6779639ab0e21ca4eb91c2ceed441a1db91efc
jigneshrb21 <jigneshrb21@gmail.com>
2026-08-08 19:55:33 +0530

    Time: 1 ms (55.49%), Space: 42.9 MB (69.44%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int n = nums.length;
+        int high = 0;
+        int low = Integer.MIN_VALUE;
+        for(int i : nums){
+            low = Math.max(low,i);
+
+            high += i;
+        }
+        int ans = -1;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            int cntk = countk(nums,k,mid);
+            if(cntk <= k){
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+    private int countk(int[] nums, int k ,int largsum){
+        int n = nums.length;
+        int subarr = 1;
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            if(sum + nums[i] <= largsum){
+                sum += nums[i];
+            }
+            else{
+                subarr++;
+                sum = nums[i];
+            }
+        }
+        return subarr;
+    }
+}
